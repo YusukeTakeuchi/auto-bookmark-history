b67cd811f4f98914603dd46bdb4d26ee7bdbe903
Yusuke Takeuchi <v.takeuci+gh@gmail.com>
2023-07-10 00:49:28 +0900

    feat: make rules configurable in the options page

--- src/bg/bg.ts
+++ src/bg/bg.ts
@@ -1,27 +1,23 @@
 import { BookmarkRuleset } from "lib/BookmarkRuleset"
-
-const BookmarkSettings = 
-  [
-    {
-      title: 'Youtube', // string
-      urlPattern: 'www.youtube.com', // string | RegExp | function
-      normalizer: (url: string) => { // RegExp | function
-        const md = /v=(\w+)/.exec(url);
-        return md ? md[0] : null;
-      },
-      maxSize: 10, // number
-    }
-  ];
-
-const ruleset = new BookmarkRuleset(BookmarkSettings);
+import { getRulesAsString } from "lib/Config";
 
 async function historyVisited(historyItem: browser.history.HistoryItem) {
-  if (historyItem.url == null || historyItem.title == null) {
-    return;
-  }
+  try {
+    if (historyItem.url == null || historyItem.title == null) {
+      return;
+    }
 
-  // @ts-ignore
-  ruleset.execute(historyItem);
+    const settingsStr = await getRulesAsString();
+    console.log({ settingsStr });
+    console.log(eval(settingsStr));
+    const ruleset = new BookmarkRuleset(eval(settingsStr));
+
+
+    // @ts-ignore
+    ruleset.execute(historyItem);
+  } catch(error) {
+    console.error( { msg: 'Error adding a bookmark', error });
+  }
 }
 
 browser.history.onVisited.addListener(historyVisited);
